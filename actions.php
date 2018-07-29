80f600b4f2a554145c124f0e4cdd218f268bce34
Paulo Gomes <pauloamgomes@gmail.com>
2018-07-29 22:00:48 +0100

    removed unused variable

--- actions.php
+++ actions.php
@@ -8,11 +8,11 @@
 $app->on('collections.find.after', function ($name, &$data) use ($app) {
   // Get the collection.
   $collection = $app->module('collections')->collection($name);
-  $publicationField = FALSE;
-  // Iterate over the collection and check that we have the field.
+  // Iterate over the collection and check that we have the publication field.
   foreach ($collection['fields'] as $field) {
     if ($field['type'] == 'publicationperiod') {
       $name = $field['name'];
+      // Field is present, check collection data entries.
       foreach ($data as $idx => $entry) {
         if (isset($entry[$name]) && !$app->module('publicationperiod')->validate($entry[$name])) {
           unset($data[$idx]);
