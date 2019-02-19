f5f2262077d1937b0c836b2ad8cf27815b5bb206
Paulo Gomes <pauloamgomes@gmail.com>
2019-02-19 16:47:41 +0000

    added acl

--- admin.php
+++ admin.php
@@ -5,6 +5,11 @@
  * Includes admin specific hooks.
  */
 
+// Module ACL definitions.
+$this("acl")->addResource('publicationperiod', [
+  'access',
+]);
+
 $app->on('admin.init', function () {
     $this->helper('admin')->addAssets('publicationperiod:assets/component.js');
     $this->helper('admin')->addAssets('publicationperiod:assets/field-publicationperiod.tag');
